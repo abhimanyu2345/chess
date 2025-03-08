6d3bf194f0fb18e66f43347958f316fb9dfae65b
Abhimanyu K Sasi <abhimanyuksasi1234@gmail.com>
2025-03-09 01:22:54 +0530

    debugging

--- chess/frontend/src/components/board.tsx
+++ chess/frontend/src/components/board.tsx
@@ -22,8 +22,8 @@ export default function ChessBoard({
   // Update clickable status whenever currentTurn or player_color changes
   useEffect(() => {
     // When it's your turn, enable pointer events; otherwise, disable clicking
-    SetClickState(currentTurn === player_color ? 'pointer-events-auto' : 'pointer-events-none');
-  }, [currentTurn, player_color]);
+    SetClickState(ChessBoard.turn()  == player_color ? 'pointer-events-auto' : 'pointer-events-none');
+  }, [ChessBoard.turn(), player_color]);
 
   const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
     if (!(e.target instanceof HTMLElement)) return;
